e77d307c100149c602a18b7b2321afb02381f77e
Prabhath565 <prabhath565@gmail.com>
2016-12-19 12:44:00 +0530

    S6_2

---
+++ external.js
@@ -0,0 +1,11 @@
+	var x = 3;   // reads the values
+	var y = 5;
+	var z = 7;
+
+	if (x > y > z) {                                      //condition for greatest number
+		console.log("Geatest number is " + x);
+	} else if (y > z > x) {
+		console.log("Geatest number is " +y);
+	} else {
+		console.log("Geatest number is " +z);
+	}
